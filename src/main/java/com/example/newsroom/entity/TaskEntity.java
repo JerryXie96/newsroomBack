040b65fd20a886b1df8975610b6056b944897481
JerryXie <xiehongcheng96@gmail.com>
2019-05-27 15:24:26 +0800

    init

---
+++ src/main/java/com/example/newsroom/entity/TaskEntity.java
@@ -0,0 +1,35 @@
+package com.example.newsroom.entity;
+
+public class TaskEntity {
+    private int stat;
+    private int flag;
+    private int page;
+
+    public TaskEntity() {
+    }
+
+
+    public int getStat() {
+        return stat;
+    }
+
+    public void setStat(int stat) {
+        this.stat = stat;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
